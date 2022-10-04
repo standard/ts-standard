76de310784fab183c106d8e9a57fcf37701569f8
Brett Logan <lindluni@github.com>
2022-10-04 09:26:59 -0400

    Add test to confirm `--version` functionality

--- test/cli.js
+++ test/cli.js
@@ -2,8 +2,10 @@ import { fileURLToPath } from 'node:url'
 
 import test from 'tape'
 import crossSpawn from 'cross-spawn'
+import options from '../options.js'
 
 const CLI_PATH = fileURLToPath(new URL('../cli.js', import.meta.url))
+const CLI_OPTIONS = fileURLToPath(new URL('../options.js', import.meta.url))
 
 test('command line usage: --help', (t) => {
   t.plan(1)
@@ -12,6 +14,23 @@ test('command line usage: --help', (t) => {
   child.on('error', (err) => {
     t.fail(err)
   })
+  child.on('close', (code) => {
+    t.equal(code, 0, 'zero exit code')
+  })
+})
+
+test('command line usage: --version', (t) => {
+  t.plan(2)
+
+  const child = crossSpawn(CLI_PATH, ['--version'])
+  child.on('error', (err) => {
+    t.fail(err)
+  })
+
+  child.stdout.on('data', (data) => {
+    t.equal(data.toString().trim(), options.version, 'version matches')
+  })
+
   child.on('close', (code) => {
     t.equal(code, 0, 'zero exit code')
   })
